082dab9c35667f8e0bec1bd69c026d129f2d2b70
Umer <umer@example.com>
2026-04-26 04:14:58 +0500

    fix config clean

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      manifest: {
-        name: 'Saad Inventory Pro',
-        short_name: 'Inventory',
-        start_url: '/',
-        display: 'standalone',
-        background_color: '#ffffff',
-        theme_color: '#000000',
-        icons: [
-          {
-            src: '/icon.png',
-            sizes: '192x192',
-            type: 'image/png'
-          }
-        ]
-      }
-    })
+    react()
   ]
 })
